a25eff7da0144ac19df7553cfa4186b9592eacb7
Muhamad Haikal Hariyanto <haikaluhuy123@gmail.com>
2024-09-06 09:49:35 +0700

    bersih

--- Pertemuan3/task6/task.java
+++ Pertemuan3/task6/task.java
@@ -5,22 +5,19 @@ public class task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Membaca dua baris input
         String inputA = scanner.nextLine();
         String inputB = scanner.nextLine();
 
-        // Mengonversi string input menjadi BigInteger
         BigInteger a = new BigInteger(inputA);
         BigInteger b = new BigInteger(inputB);
 
-        // Melakukan penjumlahan
         BigInteger sum = a.add(b);
-        // Melakukan perkalian
+
         BigInteger product = a.multiply(b);
 
-        // Mencetak hasil penjumlahan
+
         System.out.println(sum);
-        // Mencetak hasil perkalian
+
         System.out.println(product);
 
         scanner.close();
